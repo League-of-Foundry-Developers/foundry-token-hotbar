f337c4148faaec2642f4db6d5ed58716afe483e1
Stan Janssen <mail@janssen.io>
2020-06-15 17:20:26 +0200

    dev improvements

---
+++ spec/helpers/reporter.ts
@@ -0,0 +1,12 @@
+import 'jasmine';
+import { SpecReporter } from 'jasmine-spec-reporter';
+
+jasmine.getEnv().clearReporters();               // remove default reporter logs
+jasmine.getEnv().addReporter(new SpecReporter({  // add jasmine-spec-reporter
+    spec: {
+        displayPending: true,
+    },
+    summary: {
+        displayDuration: false,
+    }
+}));
