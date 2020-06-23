0d9f3a239b2d322ea474aba0eb421e6a8cabc508
Stan Janssen <mail@janssen.io>
2020-06-23 12:02:00 +0200

    Fixed migration

--- spec/flagMigration.spec.ts
+++ spec/flagMigration.spec.ts
@@ -5,6 +5,22 @@ import { TestFlaggable } from './helpers/TestToken';
 import { CONSTANTS } from '../src/utils/constants';
 
 describe('migration.translateDataStructure', function () {
+
+    it('migrates', async function () {
+        const user = new TestFlaggable('user-1');
+        const oldHotbarData: OldHotbarData = {'jUsZENMpwCBFw2Z1':[ {'slot':41,'id':'qVDGuGXCAXCOYw7R'},{'slot':43,'id':'S4P0BBEvbdELIjwU'},{'slot':45,'id':'CLX1soaLSxFDAAV8'} ],'36CpODC5K9FprGth':[ {'slot':41,'id':'qVDGuGXCAXCOYw7R'},{'slot':43,'id':'S4P0BBEvbdELIjwU'},{'slot':45,'id':'CLX1soaLSxFDAAV8'} ],'bAlcTgzHf0YlnlMz':[ {'slot':41,'id':'qVDGuGXCAXCOYw7R'},{'slot':43,'id':'S4P0BBEvbdELIjwU'},{'slot':45,'id':'CLX1soaLSxFDAAV8'} ]};
+        user.setFlag('world', 'TokenHotbar', <any>oldHotbarData);
+        const expectedData = {
+            '36CpODC5K9FprGth': { 41:'qVDGuGXCAXCOYw7R',43:'S4P0BBEvbdELIjwU',45:'CLX1soaLSxFDAAV8' },
+            'jUsZENMpwCBFw2Z1': { 41:'qVDGuGXCAXCOYw7R',43:'S4P0BBEvbdELIjwU',45:'CLX1soaLSxFDAAV8' },
+            'bAlcTgzHf0YlnlMz': { 41:'qVDGuGXCAXCOYw7R',43:'S4P0BBEvbdELIjwU',45:'CLX1soaLSxFDAAV8' }
+        };
+
+        await new Migration([ user ]).migrate();
+        console.log(user.getFlag('TokenHotbar', 'hotbar-data'));
+        expect(user.getFlag('TokenHotbar', 'hotbar-data')).toEqual(expectedData);
+    });
+
     it('changes OldHotbarData into HotbarData', function () {
         const migration = new Migration([]);
         const oldData: OldHotbarData = {
@@ -16,9 +32,10 @@ describe('migration.translateDataStructure', function () {
             'token-2': { 1: '3k46jb', 23: '7s0' }
         };
 
-        const actualData: HotbarData = migration.translateDataStructure(oldData);
-
-        expect(actualData).toEqual(expectedData);
+        for(const id in oldData) {
+            const actualData = migration.translateDataStructure(oldData[id]);
+            expect(actualData).toEqual(expectedData[id]);
+        }
     });
 
     it('unsets the HotbarData flag on world', async function () {
@@ -29,7 +46,7 @@ describe('migration.translateDataStructure', function () {
 
         const flaggable = <DataFlaggable><unknown>new TestFlaggable('some-token');
         flaggable.data = {
-            flags: { world: { [CONSTANTS.module.name]: { [flaggable.id]: oldData} } }
+            flags: { world: { [CONSTANTS.module.name]: oldData } }
         };
 
         spyOn(flaggable, 'unsetFlag');
@@ -39,13 +56,16 @@ describe('migration.translateDataStructure', function () {
         const errors = await migrator.migrate();
         errors.map(e => console.error(e));
 
+        const translatedData = Object.keys(oldData).reduce<HotbarData>( (acc, id: string) => (acc[id] = migrator.translateDataStructure(oldData[id]), acc), {});
+
         expect(errors.length).toEqual(0);
-        expect(flaggable.unsetFlag).toHaveBeenCalledTimes(2);
-        expect(flaggable.unsetFlag).toHaveBeenCalledWith(CONSTANTS.module.name, flaggable.id);
+        for(const id of Object.keys(oldData)) {
+            expect(flaggable.unsetFlag).toHaveBeenCalledWith(CONSTANTS.module.name, id);
+        }
         expect(flaggable.unsetFlag).toHaveBeenCalledWith('world', CONSTANTS.module.name);
 
         expect(flaggable.setFlag).toHaveBeenCalledTimes(1);
-        expect(flaggable.setFlag).toHaveBeenCalledWith(CONSTANTS.module.name, flaggable.id, migrator.translateDataStructure(oldData));
+        expect(flaggable.setFlag).toHaveBeenCalledWith(CONSTANTS.module.name, 'hotbar-data', translatedData);
     });
 
     it('unsets the HotbarData flag on the entity', async function () {
@@ -56,7 +76,7 @@ describe('migration.translateDataStructure', function () {
 
         const flaggable = <DataFlaggable><unknown>new TestFlaggable('some-token');
         flaggable.data = {
-            flags: { [CONSTANTS.module.name]: { [flaggable.id]: oldData} }
+            flags: { [CONSTANTS.module.name]: oldData }
         };
 
         spyOn(flaggable, 'unsetFlag');
@@ -66,12 +86,15 @@ describe('migration.translateDataStructure', function () {
         const errors = await migrator.migrate();
         errors.map(e => console.error(e));
 
+        const translatedData = Object.keys(oldData).reduce<HotbarData>( (acc, id: string) => (acc[id] = migrator.translateDataStructure(oldData[id]), acc), {});
+
         expect(errors.length).toEqual(0);
-        expect(flaggable.unsetFlag).toHaveBeenCalledTimes(2);
-        expect(flaggable.unsetFlag).toHaveBeenCalledWith(CONSTANTS.module.name, flaggable.id);
+        for(const id of Object.keys(oldData)) {
+            expect(flaggable.unsetFlag).toHaveBeenCalledWith(CONSTANTS.module.name, id);
+        }
         expect(flaggable.unsetFlag).toHaveBeenCalledWith('world', CONSTANTS.module.name);
 
         expect(flaggable.setFlag).toHaveBeenCalledTimes(1);
-        expect(flaggable.setFlag).toHaveBeenCalledWith(CONSTANTS.module.name, flaggable.id, migrator.translateDataStructure(oldData));
+        expect(flaggable.setFlag).toHaveBeenCalledWith(CONSTANTS.module.name, 'hotbar-data', translatedData);
     });
 });
--- spec/helpers/TestToken.ts
+++ spec/helpers/TestToken.ts
@@ -2,30 +2,43 @@ import { Flaggable, IToken, IActor } from '../../src/utils/foundry';
 import { HotbarData } from '../../src/flags/hotbarFlags';
 
 export class TestFlaggable implements Flaggable {
-    private flags = new Map<string, HotbarData | undefined>();
+    public data: { flags: { [scope: string]: { [key: string]: HotbarData | undefined } } }
 
-    constructor(public id: string) { }
+    constructor(public id: string) {
+        this.data = { flags: { } };
+    }
 
     getFlag(scope: string, key: string): HotbarData | undefined {
-        return this.flags.get(key);
+        const scoped = this.data.flags[scope];
+        return scoped ? scoped[key] : undefined;
     }
+
     setFlag(scope: string, key: string, data?: HotbarData): Promise<Flaggable> {
-        this.flags.set(key, data);
+        const scoped = this.data.flags[scope];
+        if (!scoped) {
+            const newScope = { [key]: data };
+            this.data.flags[scope] = newScope;
+        } else {
+            this.data.flags[scope][key] = data;
+        }
         return Promise.resolve(this);
     }
     unsetFlag(scope: string, key: string): Promise<Flaggable> {
-        this.flags.set(key, undefined);
+        const scoped= this.data.flags[scope];
+        if (scoped) {
+            delete this.data.flags[scope][key];
+        }
         return Promise.resolve(this);
     }
 }
 
 export class TestToken extends TestFlaggable implements IToken {
-    public data: { actorLink: boolean };
+    public data: { flags: { [scope: string] : { [key: string] : HotbarData | undefined } }, actorLink: boolean };
     public actor?: IActor;
 
     constructor(id: string, actor: IActor | undefined) {
         super(id);
-        this.data = { actorLink: !!actor };
+        this.data['actorLink'] = !!actor;
         this.actor = actor;
     }
 }
